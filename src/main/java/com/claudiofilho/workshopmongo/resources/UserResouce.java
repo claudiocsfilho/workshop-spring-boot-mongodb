688cbc9791982814398690e0053d593fefdd5977
Claudio Filho <claudiocsfilho@gmail.com>
2025-02-07 12:03:35 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/claudiofilho/workshopmongo/resources/UserResouce.java
+++ src/main/java/com/claudiofilho/workshopmongo/resources/UserResouce.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.claudiofilho.workshopmongo.domain.Post;
 import com.claudiofilho.workshopmongo.domain.User;
 import com.claudiofilho.workshopmongo.dto.UserDTO;
 import com.claudiofilho.workshopmongo.services.UserService;
@@ -59,6 +60,12 @@ public class UserResouce {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
