b4468703ca6e8be064f8f08ca72be7065046e720
Claudio Filho <claudiocsfilho@gmail.com>
2025-02-07 15:57:58 -0300

    Consulta simples com @Query

--- src/main/java/com/claudiofilho/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/claudiofilho/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.claudiofilho.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.claudiofilho.workshopmongo.domain.Post;
@@ -13,5 +14,8 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	//QUERY Methods
 	//Apenas esta declaração faz com que o Spring data faça a consulta.
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 	
 }
